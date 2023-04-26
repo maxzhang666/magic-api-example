6e961e72411b3957de464db8cded7115638ba7aa
MaxZhang <373884384@qq.com>
2023-04-26 16:42:24 +0800

    增加定时任务、es、Redis插件

--- src/main/java/org/ssssssss/example/MagicAPIExampleApplication.java
+++ src/main/java/org/ssssssss/example/MagicAPIExampleApplication.java
@@ -2,15 +2,16 @@ package org.ssssssss.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SpringDataWebAutoConfiguration.class)
 //@EnableSwagger2	// 配置swagger 文档
 public class MagicAPIExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MagicAPIExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MagicAPIExampleApplication.class, args);
+    }
 }
 
